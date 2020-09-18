d171b1627c6089f526adc78ab72df2b614ad0c99
ricardotiongson <ricardo.tiongson190151@my.lsgh.edu.ph>
2020-09-18 20:31:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-irhzu1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-irhzu1)
---
+++ pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css'],
+})
+export class HomePage {
+  initValue: number;
+  depValue: number;
+
+  constructor(public navCtrl: NavController) {
+  }
+
+compute () {
+    this.depValue = (this.initValue * 0.40);
+  }
+
+}
